16dba3f0ba9609e8513ced71f81781fd2aac3eeb
bitcoinfees <bitcoinfees@gmail.com>
2016-02-27 13:06:41 +0800

    fix gitsetupremote

--- fish/functions/gitsetupremote.fish
+++ fish/functions/gitsetupremote.fish
@@ -1,9 +1,14 @@
 function gitsetupremote
-    if count $argv < 2
+    set numargs (count $argv)
+    if math "$numargs<2" > /dev/null
         echo "gitsetupremote <name> <remoteip>"; return
+    end
     git clone --bare . $argv[1].git
     and rsync -aP $argv[1].git git@$argv[2]:.
     and git remote add origin ssh://git@$argv[2]/home/git/$argv[1].git
-    and rm -rf $argv[1].git
     and echo "All OK"
+
+    if [ -d $argv[1].git ]
+        rm -rf $argv[1].git
+    end
 end
