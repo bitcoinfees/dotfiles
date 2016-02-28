9b0f77bad7f92212b8f4d17d9033d44d68a2a11f
bitcoinfees <bitcoinfees@gmail.com>
2016-02-28 15:53:32 +0800

    fish source

--- fish/functions/fish_edit_fn.fish
+++ fish/functions/fish_edit_fn.fish
@@ -1,4 +1,4 @@
-function fish_fn_edit --description 'Edit a fish fn.'
+function fish_edit_fn --description 'Edit a fish fn.'
     set name $argv[1]
     if [ -z $name ]
         echo "No function name specified!"
@@ -7,9 +7,7 @@ function fish_fn_edit --description 'Edit a fish fn.'
 
     set funcfile $FISH_HOME/functions/$name.fish
     if [ ! -e $funcfile ]
-        # echo function $name\nend >> $funcfile
-        echo "No such function!"
-        return 1
+        echo function $name\nend >> $funcfile
     end
     eval $EDITOR $funcfile
 
---
+++ fish/functions/fish_source.fish
@@ -0,0 +1,3 @@
+function fish_source -d "Source config.fish"
+    source $FISH_HOME/config.fish
+end
