675c02eea0903b386004b40668416e048f1015d7
bitcoinfees <bitcoinfees@gmail.com>
2016-03-17 18:12:03 +0800

    fish_edit_fn improvment + vimrc go info map

--- fish/functions/fish_edit_fn.fish
+++ fish/functions/fish_edit_fn.fish
@@ -7,12 +7,12 @@ function fish_edit_fn --description 'Edit a fish fn.'
 
     set funcfile $FISH_HOME/functions/$name.fish
     if [ ! -e $funcfile ]
-        echo function $name\nend >> $funcfile
+        echo \#function $name\n\#end >> $funcfile
     end
     eval $EDITOR $funcfile
 
-    # Remove file if it's empty
-    if not grep '\S' $funcfile > /dev/null
+    # Remove file if it's empty apart from comments
+    if not begin; grep -v '^\s*#' $funcfile | grep '\S'; end > /dev/null
         rm $funcfile
     end
 end
