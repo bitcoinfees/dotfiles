d484f8418accb69bb9e25e66aed8e970bb3fb2bc
bitcoinfees <bitcoinfees@gmail.com>
2016-05-06 18:52:22 +0800

    gitsetupremote allow specifying remote name

--- fish/functions/gitsetupremote.fish
+++ fish/functions/gitsetupremote.fish
@@ -1,11 +1,17 @@
 function gitsetupremote
     set numargs (count $argv)
     if math "$numargs<2" > /dev/null
-        echo "gitsetupremote <name> <remoteip>"; return
+        echo "gitsetupremote <name> <remoteip> [remote name]"; return
+    end
+    if math "$numargs>2" > /dev/null
+        set remotename $argv[3]
+    else
+        echo 'Remote name not specified, defaulting to "origin"'
+        set remotename origin
     end
     git clone --bare . $argv[1].git
     and rsync -aP $argv[1].git git@$argv[2]:.
-    and git remote add origin ssh://git@$argv[2]/home/git/$argv[1].git
+    and git remote add $remotename ssh://git@$argv[2]/home/git/$argv[1].git
     and echo "All OK"
 
     if [ -d $argv[1].git ]
